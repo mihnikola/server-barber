35d0704b14f0c3ceeaf6645a9f37ccd7c387dd37
nikolami993 <123368077+nikolami993@users.noreply.github.com>
2025-05-13 20:45:11 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 // Create a new service
-// router.post('/', userController.createUser);
+router.post('/', userController.createUser);
 // router.post('/admin', userController.createAdminUser);
 router.post('/login', userController.loginUser);
 // router.post('/login/admin', userController.loginAdminUser);
