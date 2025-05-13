91234d355c450acdeeb98cf4325a2eaf6cf49922
nikolami993 <123368077+nikolami993@users.noreply.github.com>
2025-05-13 13:45:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.json({ limit: "50mb" }));
-
 app.use(express.static('public'));
 app.use('images', express.static('images'));
 
