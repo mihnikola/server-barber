497a1891c1ebe4c55ac21502c95cd2c73287ca03
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-05-13 13:03:06 +0200

    Delete routes directory

--- routes/tokenRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const tokenController = require('../controllers/tokenController');
-
-const router = express.Router();
-
-router.post('/send', tokenController.sendNotification);
-router.post('/saveToken',tokenController.saveToken);
-
-module.exports = router;
