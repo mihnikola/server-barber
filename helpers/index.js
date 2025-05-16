e6056cc988b702d15f2d7fee2ee116631da5bbd6
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-05-16 12:03:04 +0200

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { Readable } = require("stream"); // This imports the Readable stream correctly
 
-
 // Your base64-encoded image (for example, a PNG image)
 function base64ToUriFunc(base64String) {
   // Convert the base64 string to a buffer
@@ -81,7 +80,7 @@ const authenticate = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (err) {
-    console.log("authenticate",err)
+    console.log("authenticate", err);
     res.status(400).send("Invalid token");
   }
 };
@@ -105,14 +104,14 @@ const convertToTimeStamp = (dateStr, timeStr) => {
 const convertToDateFormat = (dateStr) => {
   // Parse the input date string to a Date object
   return dateStr.split("T")[0]; // Replace 'Z' with '+00:00'
-}
+};
 
-const  getDateRange = (dateString) => {
+const getDateRange = (dateString) => {
   const start = new Date(dateString + "T00:00:00.000Z");
   const end = new Date(start);
   end.setUTCDate(end.getUTCDate() + 1);
   return { start, end };
-}
+};
 
 function toLocaleDateTimeString(utcString) {
   const date = new Date(utcString);
@@ -131,16 +130,122 @@ const filterFutureTimeSlots = (timeSlots, currentTime, dateValue) => {
 
   return timeSlots.filter((timeSlot) => {
     const [hours, minutes] = timeSlot.value.split(":").map(Number);
-    slotTime.setHours(hours);
-    slotTime.setMinutes(minutes);
-    slotTime.setSeconds(0);
-    slotTime.setMilliseconds(0);
+    slotTimeLocale.setHours(hours);
+    slotTimeLocale.setMinutes(minutes);
+    slotTimeLocale.setSeconds(0);
+    slotTimeLocale.setMilliseconds(0);
 
-    return slotTime > currentTime;
+    return slotTimeLocale > currentTime;
   });
 };
 
+function updateTimeToTenUTC(dateString, timeString) {
+  const datePart = dateString.substring(0, 10);
+  const desiredUTCTime = `${timeString}:00.000+00:00`;
+  const newUTCDateString = `${datePart}T${desiredUTCTime}`;
+  return newUTCDateString;
+}
+
+function convertSlashDateToSerbianFormat(slashDate) {
+
+  if (slashDate.includes("/")) {
+    const parts = slashDate.split("/");
+    if (parts.length === 3) {
+      const month = parts[0].padStart(2, "0");
+      const day = parts[1].padStart(2, "0");
+      const year = parts[2];
+      return `${day}.${month}.${year}`;
+    } else {
+      return "Invalid date format provided. Expected MM/DD/YYYY or similar.";
+    }
+  } else {
+    const parts = slashDate.split(" ");
+    const month = parts[0].split(".")[0];
+    const day = parts[1].split(".")[0];
+    const year = parts[2];
+    return `${day}.${month}.${year}`;
+  }
+}
+
+function convertSerbianDateTimeToUTCWithSplitJoin(dateString, timeString) {
+  const localeDateTimeString = `${dateString} ${timeString}`;
+  if (!localeDateTimeString) {
+    return null;
+  }
+
+  console.log("localeDateTimeString", localeDateTimeString);
+
+  // Split the localeDateTimeString
+  const dateAndTimeParts = localeDateTimeString?.split(" "); // Split date and time
+  let day = dateAndTimeParts[0]?.split(".")[0];
+  let month = dateAndTimeParts[0]?.split(".")[1];
+  let year = dateAndTimeParts[0]?.split(".")[2];
+
+  let hour = dateAndTimeParts[1]?.split(":")[0];
+  let minute = dateAndTimeParts[1]?.split(":")[1];
+  let second = dateAndTimeParts[1]?.split(":")[2];
+
+  // Parse day, month, year, hour, minute, and second directly
+  day = parseInt(day, 10);
+  month = parseInt(month, 10) - 1; // Month is 0-indexed
+  year = parseInt(year, 10);
+  hour = parseInt(hour, 10);
+  minute = parseInt(minute, 10);
+  second = parseInt(second, 10);
+
+  //check if parsing was successful
+  if (
+    isNaN(day) ||
+    isNaN(month) ||
+    isNaN(year) ||
+    isNaN(hour) ||
+    isNaN(minute) ||
+    isNaN(second)
+  ) {
+    console.log("first", year, month, day, hour, minute, second);
+    console.error(`Invalid date/time value in: ${localeDateTimeString}`);
+    return null;
+  }
+  const localDate = new Date(year, month, day, hour, minute, second);
+  const utcDate = new Date(
+    localDate.getTime() - localDate.getTimezoneOffset() * 60 * 1000
+  );
+  const utcTimeString = utcDate.toISOString();
+  return utcTimeString;
+}
+
+function formatAndConvertToSerbian(dateTimeString) {
+  const parts = dateTimeString.split(" ");
+  const timePart = parts[0];
+  const ampm = parts[1];
+
+  let hour = parseInt(timePart.split(":")[0], 10);
+  const minute = parseInt(timePart.split(":")[1], 10);
+  let second = parseInt(timePart.split(":")[2], 10);
+
+  // Convert AM/PM to 24-hour format
+  if (ampm === "PM" && hour < 12) {
+    hour += 12;
+  } else if (ampm === "AM" && hour === 12) {
+    hour = 0;
+  }
+  // Change the  seconds to 28
+  second = 28;
+
+  // Format the time string
+  const formattedHour = String(hour).padStart(2, "0");
+  const formattedMinute = String(minute).padStart(2, "0");
+  const formattedSecond = String(second).padStart(2, "0");
+  const formattedTime = `${formattedHour}:${formattedMinute}:${formattedSecond}`;
+
+  return formattedTime;
+}
+
 module.exports = {
+  updateTimeToTenUTC,
+  convertSerbianDateTimeToUTCWithSplitJoin,
+  formatAndConvertToSerbian,
+  convertSlashDateToSerbianFormat,
   convertToDateFormat,
   getDateRange,
   getTimeValues,
@@ -151,5 +256,5 @@ module.exports = {
   base64ToUriFunc,
   prettyUrlDataImage,
   convertToTimeStamp,
-  filterFutureTimeSlots
+  filterFutureTimeSlots,
 };
