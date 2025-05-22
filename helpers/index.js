2122c9ab64adf4160702228b5f81301322e18cbd
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-05-22 08:12:23 +0200

    Update index.js

--- helpers/index.js
+++ helpers/index.js
@@ -146,6 +146,7 @@ function updateTimeToTenUTC(dateString, timeString) {
   return newUTCDateString;
 }
 function convertToISO8601(dateInput) {
+
   // Helper function to parse DD/MM/YYYY, HH:mm:ss format
   function parseSlashSeparatedDateTime(inputString) {
     const match = inputString.match(
@@ -177,9 +178,11 @@ function convertToISO8601(dateInput) {
         const dayValue = day.toString().length > 1 ? day : `0${day}`;
         const minuteValue = minute.toString().length > 1 ? minute : `0${minute}`;
         const hourValue = hour.toString().length > 1 ? hour : `0${hour}`;
+        const secondValue = second.toString().length > 1 ? second : `0${second}`;
 
         // Create a Date object. Month is 0-indexed in JavaScript Date constructor.
-        return `${year}-${monthValue}-${dayValue}T${hourValue}:${minuteValue}:${second}`;
+        
+        return `${year}-${monthValue}-${dayValue}T${hourValue}:${minuteValue}:${secondValue}`;
       }
     }
     return null;
@@ -211,8 +214,16 @@ function convertToISO8601(dateInput) {
         second >= 0 &&
         second <= 59
       ) {
+
+        const monthValue = month.toString().length > 1 ? month : `0${month}`;
+        const dayValue = day.toString().length > 1 ? day : `0${day}`;
+        const minuteValue = minute.toString().length > 1 ? minute : `0${minute}`;
+        const hourValue = hour.toString().length > 1 ? hour : `0${hour}`;
+        const secondValue = second.toString().length > 1 ? second : `0${second}`;
+
         // Create a Date object. Month is 0-indexed in JavaScript Date constructor.
-        return `${year}-${month}-${day}T${hour}:${minute}:${second}`;
+        return `${year}-${monthValue}-${dayValue}T${hourValue}:${minuteValue}:${secondValue}`;
+        // Create a Date object. Month is 0-indexed in JavaScript Date constructor.
       }
     }
     return null;
