7837d12d51949369d3595dfe76d2bb929da3d06f
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-05-28 08:23:54 +0200

    Update server.js prvo DB connection

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+
+// --- Konfiguracija Express aplikacije ---
 app.use(cors());
+
+// Middleware za parsiranje JSON i URL-enkodiranih tela
+
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+// Serviranje statičkih fajlova
+app.use(express.static('public')); // Za fajlove u 'public' folderu
+app.use('/images', express.static('images')); // Za fajlove u 'images' folderu, dostupne preko /images URL-a
+
+// --- Importovanje ruta ---
 const tokenRoutes = require('./routes/tokenRoutes');
 const userRoutes = require("./routes/userRoutes");
 const serviceRoutes = require("./routes/serviceRoutes");
@@ -11,44 +24,7 @@ const reservationRoutes = require("./routes/reservationRoutes");
 const notificationsRoutes = require("./routes/notificationRoutes");
 const timesRoutes = require("./routes/timesRoutes");
 
-// const connectDB = require('./connectDB.js');
-// connectDB();
-
-
-
-
-
-
-async function connectDB() {
-  const uri =
-  "mongodb+srv://root:3Slap3hMgxH8V3pm@cluster0.szwql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-  try {
-    await mongoose.connect(uri,{
-        serverSelectionTimeoutMS: 30000
-
-    });
-    console.log("Database connected successfully to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
-
-
-
-
-// Middleware to parse JSON
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
-app.use(express.json({ limit: "50mb" }));
-
-app.use(express.static('public'));
-app.use('images', express.static('images'));
-
-app.get('/',  (req, res) => {
- connectDB();
-})
-
+// --- Definisanje ruta ---
 app.use('/api',tokenRoutes);
 app.use("/users", userRoutes);
 app.use("/services", serviceRoutes);
@@ -56,9 +32,55 @@ app.use("/reservations", reservationRoutes);
 app.use("/times", timesRoutes);
 app.use("/notifications", notificationsRoutes);
 
-// Start server
+// --- Funkcija za povezivanje sa bazom podataka ---
+async function connectDB() {
+  const uri =
+    "mongodb+srv://root:3Slap3hMgxH8V3pm@cluster0.szwql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  try {
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 30000 // Daje više vremena za odabir servera
+    });
+    console.log("Database connected successfully to MongoDB");
+    return true; // Vraća true ako je konekcija uspela
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    return false; // Vraća false ako je došlo do greške
+  }
+}
+
+// --- Glavna funkcija za pokretanje servera ---
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  
-  console.log(`Server running on port ${PORT}`);
-});
+
+async function startApp() {
+  const dbConnected = await connectDB(); // Pokušaj konekcije sa bazom podataka
+
+  if (dbConnected) {
+    // Ako je konekcija sa bazom uspela, pokreni server
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } else {
+    
+    console.error("Server not started due to database connection failure.");
+    process.exit(1) 
+  }
+}
+
+
+startApp();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
