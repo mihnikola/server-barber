644db0059f491bf31eb6c4542638412da8dd9fdd
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-06-05 16:05:29 +0200

    Update tokenController.js

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -3,7 +3,7 @@ const Token = require("../models/Token");
 // API route to send token
 
 exports.sendNotification = async (req, res) => {
-  const { token, title, content,data } = req.body;
+  const { token, title, content, data } = req.body;
 
   const message = {
     to: token,
@@ -35,18 +35,43 @@ exports.sendNotification = async (req, res) => {
     });
 };
 // API route to save token
+async function updateTokenFirebase(userId, token) {
+  const functionUrl =
+    "https://us-central1-barberappointmentapp-85deb.cloudfunctions.net/updateTokenExpoPushToFirestore";
+  await axios
+    .post(functionUrl, { userId, token })
+    .then((res) => {
+      console.log("updateTokenFirebase solve", res.data.message);
+    })
+    .catch((err) => {
+      console.log("err", err);
+    });
+}
 
 exports.saveToken = async (req, res) => {
   const { tokenExpo, tokenUser } = req.body;
-  try {
-    const tokens = await Token.findOne({ token: tokenExpo });
-    if (tokens) {
-      return res.status(200).send({ status: 200, message: "Token already exist"});
-    }
-    const newToken = new Token({ token: tokenExpo, user: tokenUser });
-    await newToken.save();
 
-    res.status(200).send({ status: 200, message: "Token saved successfully" });
+  try {
+    const userData = await Token.findOne({ user: tokenUser });
+    if (userData) {
+      await Token.findOneAndUpdate(
+        { user: tokenUser },
+        { $set: { token: tokenExpo } },
+        { new: true }
+      );
+      updateTokenFirebase(tokenUser, tokenExpo);
+
+      return res
+        .status(200)
+        .send({ status: 200, message: "Token updated successfully" });
+    } else {
+      const newToken = new Token({ token: tokenExpo, user: tokenUser });
+      await newToken.save();
+
+      return res
+        .status(200)
+        .send({ status: 200, message: "Token saved successfully" });
+    }
   } catch (error) {
     res
       .status(500)
