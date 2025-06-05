2e74b7e5f2757afb63ed87998b9aac64bc32caac
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-06-05 16:05:46 +0200

    Update userController.js2

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,10 +33,8 @@ exports.createAdminUser = async (req, res) => {
     });
     await newUser.save();
     res.status(201).json({
-      message:
-        "User created successfully!",
+      message: "User created successfully!",
     });
-   
   } catch (err) {
     console.error("Error:", err); // Log error to console
     res.status(500).json({ error: err.message });
@@ -62,7 +60,6 @@ exports.createUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-
     const newUser = new User({
       name,
       email,
@@ -70,13 +67,16 @@ exports.createUser = async (req, res) => {
       isVerified: true,
     });
     await newUser.save();
- 
+
+   
+
     res.status(201).json({
-      message:
-        "User created successfully!",
+      message: "User created successfully!",
     });
   } catch (err) {
-    res.status(500).send({ status: 500, message: "Something Went Wrong, Please Try Again" });
+    res
+      .status(500)
+      .send({ status: 500, message: "Something Went Wrong, Please Try Again" });
   }
 };
 
@@ -113,8 +113,6 @@ exports.createUser = async (req, res) => {
 //     res.status(500).json({ error: "Failed to verify the token." });
 //   }
 // };
-
-
 
 //loginAdminUser
 exports.loginAdminUser = async (req, res) => {
@@ -163,40 +161,47 @@ exports.loginAdminUser = async (req, res) => {
 
 // Login user
 exports.loginUser = async (req, res) => {
-
   try {
     const { email, password } = req.body;
     // Find the user in the "database"
     const user = await User.findOne({ email }); // `findOne` is typically better if you expect a single result
 
     if (!user) {
-      return res.status(202).json({ status:202, message: "Incorrect email or password" });
+      return res
+        .status(202)
+        .json({ status: 202, message: "Incorrect email or password" });
     }
 
     // Compare the password with the hashed password stored in the database
     const isPasswordMatch = await bcrypt.compare(password, user.password);
 
     if (!isPasswordMatch) {
-      return res.status(202).json({ status:202, message: "Incorrect password" });
+      return res
+        .status(202)
+        .json({ status: 202, message: "Incorrect password" });
     }
 
     if (!user.isVerified) {
-      return res.status(202).json({ status:202, message: "Not verified" });
+      return res.status(202).json({ status: 202, message: "Not verified" });
     }
     const userData = {
       id: user._id.toHexString(),
       email: user.email,
     };
+
+
+
     // Create JWT token
     const token = jwt.sign(userData, process.env.JWT_SECRET_KEY, {
       expiresIn: "10000000m",
     });
     res.status(200).json({ status: 200, token, userId: user._id });
   } catch (err) {
-    res.status(500).send({ status: 500, message: "Something Went Wrong, Please Try Again" });
+    res
+      .status(500)
+      .send({ status: 500, message: "Something Went Wrong, Please Try Again" });
   }
 };
-
 
 exports.getUsers = async (req, res) => {
   try {
@@ -206,11 +211,10 @@ exports.getUsers = async (req, res) => {
         id: user._id,
         name: user.name,
         role: user.role,
-        image: prettyUrlDataImage(`${process.env.API_URL}/${user.image}`)
+        image: prettyUrlDataImage(`${process.env.API_URL}/${user.image}`),
       };
     });
-      res.status(200).json({ status: 200, data: usersData });
-
+    res.status(200).json({ status: 200, data: usersData });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -222,29 +226,29 @@ exports.getUser = async (req, res) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     const user = await User.findOne({ _id: decoded.id });
 
-    console.log("ttttttttt",user)
+    console.log("ttttttttt", user);
 
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
-exports.patchUser = async (req,res) =>{
-    try {
-      const { id } = req.params;
-      const { name, image } = req.body.params;
-  
-      const user = await User.findByIdAndUpdate(
-        id,
-        { name, image },
-        { new: true }
-      );
-      if (!user) {
-        return res.status(404).send("User not found");
-      }
-  
-      res.status(200).json({ message: "User updated successfully" });
-    } catch (error) {
-      res.status(500).send("Server Error");
+exports.patchUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, image } = req.body.params;
+
+    const user = await User.findByIdAndUpdate(
+      id,
+      { name, image },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).send("User not found");
     }
-  };
+
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+};
