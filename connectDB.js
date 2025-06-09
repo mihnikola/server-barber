80bcc7f1a958107edf19584d16515222d2d41f7e
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-06-09 14:52:23 +0200

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose");
-const uri =
-  "mongodb+srv://root:3Slap3hMgxH8V3pm@cluster0.szwql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+import mongoose from "mongoose";
 
-async function connectDB() {
+// --- Funkcija za povezivanje sa bazom podataka ---
+const connectDB = async () => {
+  const uri =
+    "mongodb+srv://root:3Slap3hMgxH8V3pm@cluster0.szwql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   try {
-    await mongoose.connect(uri,{
-        serverSelectionTimeoutMS: 10000
-
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 30000, // Daje više vremena za odabir servera
     });
     console.log("Database connected successfully to MongoDB");
+    return true; // Vraća true ako je konekcija uspela
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
+    return false; // Vraća false ako je došlo do greške
   }
-}
+};
 
-module.exports = connectDB;
+export default connectDB;
