16e0d612c1e5a27e9197db22039f21f1468f6674
mihnikola <47032376+mihnikola@users.noreply.github.com>
2025-07-27 13:05:42 +0200

    Update mail.js axios post

--- helpers/mail.js
+++ helpers/mail.js
@@ -1,30 +1,24 @@
+const { default: axios } = require("axios");
+
 const sendEmail = async (receipients) => {
-  const functionUrl = "https://us-central1-barberappointmentapp-85deb.cloudfunctions.net/sendMail"; 
+  const functionUrl =
+    "https://us-central1-barberappointmentapp-85deb.cloudfunctions.net/sendMail";
 
-  try {
-    const response = await fetch(functionUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        to: receipients.receipients,
-        subject: receipients.subject,
-        html: receipients.message,
-        text: receipients.message,
-      }),
-    });
-
-    if (response.ok) {
+  await axios
+    .post(functionUrl, {
+      to: receipients.receipients,
+      subject: receipients.subject,
+      html: receipients.message,
+      text: receipients.message,
+    })
+    .then((res) => {
+      console.log("solve", res.data.message);
       console.log("Email je uspešno poslat!");
-    } else {
-      const errorText = await response.text();
+    })
+    .catch((err) => {
+      console.log("err", err);
       console.error("Greška pri slanju emaila:", errorText);
-    }
-  } catch (error) {
-    console.error("Došlo je do greške u mreži:", error);
-  }
+    });
 };
-
 
 module.exports = { sendEmail };
