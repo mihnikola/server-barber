c91a37923956137c976916cd1ef6ea8bf1a9ad1e
mihnikola <mihailovic93.nikola@gmail.com>
2025-08-20 22:33:54 +0200

    qqqqq

--- controllers/userController.js
+++ controllers/userController.js
@@ -370,7 +370,7 @@ export const loginUser = async (req, res) => {
       return res.status(202).json({
         status: 606,
         message:
-          "Your account is not verify yet, otp code will be send to your email.",
+          "Your account is not verified yet. Verification code will be sent to your email.",
       });
     }
     const userData = {
