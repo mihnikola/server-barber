8adaaf637ee8ef829bc873953cfc6be83f9646d2
mihnikola <mihailovic93.nikola@gmail.com>
2025-08-28 16:15:47 +0200

    getReservations

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -102,66 +102,13 @@ exports.getReservations = async (req, res) => {
 
     const customerId = decoded.id;
 
-    const now = new Date();
-
-    // Set the time zone to your specific location
-    const options = {
-      timeZone: "Europe/Belgrade", // This covers Serbia
-      day: "2-digit",
-      month: "2-digit",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false, // Use 24-hour format
-    };
-
-    const localDateString = now.toLocaleString("en-GB", options);
-    const [datePart, timePart] = localDateString.split(", ");
-    const [day, month, year] = datePart.split("/").map(Number);
-    const [hours, minutes, seconds] = timePart.split(":").map(Number);
-
-    const monthValue = month.toString().length > 1 ? month : `0${month}`;
-    const dayValue = day.toString().length > 1 ? day : `0${day}`;
-    const minuteValue = minutes.toString().length > 1 ? minutes : `0${minutes}`;
-    const hourValue = hours.toString().length > 1 ? hours : `0${hours}`;
-    const secondValue = seconds.toString().length > 1 ? seconds : `0${seconds}`;
-
-    // Create a Date object. Month is 0-indexed in JavaScript Date constructor.
-    const dateValue = `${year}-${monthValue}-${dayValue}T${hourValue}:${minuteValue}:${secondValue}.000+00:00`;
-
-    console.log("ISO 8601:", dateValue); // Output: 2025-08-23T14:43:56.000Z
-
-    const futureReservations = await Reservation.find({
+    const reservations = await Reservation.find({
       user: customerId,
-      date: { $gt: dateValue },
       status: { $nin: [2] },
     })
       .sort({ date: 1 })
       .populate("service")
       .populate("employer");
-
-    const pastReservations = await Reservation.find({
-      user: customerId,
-      date: { $lt: dateValue },
-      status: { $nin: [2] },
-    })
-      .sort({ date: -1 })
-      .populate("service")
-      .populate("employer");
-
-    const modifiedPastReservations = pastReservations?.map((reservation) => {
-      const reservationObj = reservation.toObject();
-      return { ...reservationObj, past: true };
-    });
-    const reservations = [...futureReservations, ...modifiedPastReservations];
-
-    console.log("futureReservations", futureReservations);
-    console.log("modifiedPastReservations", modifiedPastReservations);
-    console.log("pastReservations", pastReservations);
-
-    console.log("reservations", reservations);
-
     res.status(200).json(reservations);
   } catch (err) {
     console.log("errorcina", err);
