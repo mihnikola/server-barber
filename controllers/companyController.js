37ee79b1402bf59134649ec0a5b9a8a07db66ad1
mihnikola <mihailovic93.nikola@gmail.com>
2025-09-25 08:31:59 +0200

    merge

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -31,6 +31,7 @@ exports.getCompany = async (req, res) => {
       },
       reviews,
     };
+    console.log("lang company", lang,company)
     res.status(200).json(company);
   } catch (err) {
     res.status(500).json({ message: err.message });
