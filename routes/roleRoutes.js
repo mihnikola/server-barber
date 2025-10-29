658598803b5082873a2c654bc13ef08c1b81529e
mihnikola <mihailovic93.nikola@gmail.com>
2025-10-30 00:17:37 +0100

    no auth

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -5,6 +5,7 @@ const { authenticate } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Create a new service
-router.get('/',authenticate, roleController.getRoles);
+// router.get('/',authenticate, roleController.getRoles);
+router.get('/', roleController.getRoles);
 
 module.exports = router;
--- routes/timesRoutes.js
+++ routes/timesRoutes.js
@@ -5,6 +5,7 @@ const { authenticate } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // Get all services
-router.get('/', authenticate, timeController.getTimes);
+// router.get('/', authenticate, timeController.getTimes);
+router.get('/', timeController.getTimes);
 
 module.exports = router;
